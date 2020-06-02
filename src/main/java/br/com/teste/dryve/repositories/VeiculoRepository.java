295dd75ab5a3e5cc922dcd7a2ff59433bf689636
Felipe Yuri Silva <felipe.yuri.silva@everis.com>
2020-06-02 17:00:25 -0300

    Início dev do desafio 2

---
+++ src/main/java/br/com/teste/dryve/repositories/VeiculoRepository.java
@@ -0,0 +1,9 @@
+package br.com.teste.dryve.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.teste.dryve.entities.Veiculo;
+
+public interface VeiculoRepository extends CrudRepository<Veiculo, Long>{
+
+}
